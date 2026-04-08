15b655e475c1e597f596b348914a25cd5500e93c
Efraim Negreiros <efraimjnegreiross@gmail.com>
2026-04-08 13:10:33 -0300

    Update usuario.model.js

--- server/models/usuario.model.js
+++ server/models/usuario.model.js
@@ -4,14 +4,48 @@ const sequelize = require('../config/database');
 
 const Usuario = sequelize.define('Usuario', {
   nome: { type: DataTypes.STRING, allowNull: false },
+
   tipo: { 
-    type: DataTypes.ENUM('membro', 'tesouraria', 'lider', 'coordenador', 'lider de eventos', 'secretaria'), 
+    type: DataTypes.ENUM(
+      'membro', 
+      'tesouraria', 
+      'lider', 
+      'coordenador', 
+      'lider de eventos', 
+      'secretaria'
+    ), 
     allowNull: false 
   },
+  imgComponente: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    defaultValue: null
+  },
+
   email: { type: DataTypes.STRING, allowNull: true, unique: true },
   senha: { type: DataTypes.STRING, allowNull: true },
+
   dataNascimento: { type: DataTypes.DATEONLY, allowNull: false },
-  naipe: { type: DataTypes.ENUM('homem', 'mulher'), allowNull: false },
+
+  naipe: { 
+    type: DataTypes.ENUM(
+      'homem', 
+      'mulher', 
+      'baixo', 
+      'tenor', 
+      'contralto', 
+      'soprano'
+    ), 
+    allowNull: false 
+  },
+
+  // 👉 TOKEN PARA NOTIFICAÇÃO EXPO (WEB + MOBILE)
+  expoPushToken: { 
+    type: DataTypes.STRING,
+    allowNull: true,
+    defaultValue: null,
+  },
+
 }, {
   tableName: 'usuarios',
   timestamps: true
